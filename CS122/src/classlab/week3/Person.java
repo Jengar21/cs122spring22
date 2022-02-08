801d23db67b57b71a5ffaf6ae067ca68ccf04773
Jennifer Garcia <arisjennifer21@gmail.com>
2022-02-08 11:44:39 -0500

    classlab week 3

---
+++ CS122/src/classlab/week3/Person.java
@@ -0,0 +1,24 @@
+package classlab.week3;
+
+public abstract class Person {
+	public String name;
+
+	private int age;
+
+	public int getAge() {
+		return getAge();
+	}
+
+	public int setAge() {
+		return setAge();
+	}
+
+	protected String city;
+
+	public abstract void printName();
+	
+	protected abstract void printAge();
+
+	//public static void printObjValues() will give you can error 
+
+}
---
+++ CS122/src/classlab/week3/PersonDriver.java
@@ -0,0 +1,13 @@
+package classlab.week3;
+
+
+public class PersonDriver {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.printName();
+		s.printAge();
+	}
+
+}
+
+
