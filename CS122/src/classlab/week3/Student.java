45b202ecb6096a8a789e6c946fca4108423551a9
Jennifer Garcia <arisjennifer21@gmail.com>
2022-02-11 13:04:55 -0500

    Update Student.java

--- CS122/src/classlab/week3/Student.java
+++ CS122/src/classlab/week3/Student.java
@@ -2,14 +2,12 @@ package classlab.week3;
 
 public class Student extends Person {
 	public String major;
-	@Override
+
 	public void printName() {
-		// TODO Auto-generated method stub
 		
 	}
-	@Override
+	
 	protected void printAge() {
-		// TODO Auto-generated method stub
 		
 	}
 
