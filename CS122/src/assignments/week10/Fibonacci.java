aee06107b3c2dab33208dfcf23b25e6022627d39
Jennifer Garcia <arisjennifer21@gmail.com>
2022-03-29 12:03:05 -0400

    week 10

---
+++ CS122/src/assignments/week10/Fibonacci.java
@@ -0,0 +1,40 @@
+package assignments.week10;
+
+public class Fibonacci {
+
+	//fib(0)=0;
+	//fib(1)=1;
+	//fib(index)= fib(index -1) + fib(index-2);
+
+	public static int fib(int num) {
+		int result;
+		if(num==0 || num==1) {
+			//System.out.print(num + "\t");
+			return 1;
+		}
+		else {
+			result= fib(num-1) + fib(num-2);
+			//System.out.print(result +"\t");
+			return result;
+
+		}
+	}
+	public static void main( String[] args) {
+		for(int i =0; i<12; i++) {
+			System.out.print(fib(i)+ "\t");
+		}
+
+		public static boolean isPalindrome(String s) {
+			if(s.length()<=1)
+				return true;
+			else if (s.charAt(0) != s.charAt(s.length()-1))
+				return false;
+			else
+				return isPalindrome(s.substring(1, s.length()-1));
+			
+
+		}
+
+	}
+
+}
