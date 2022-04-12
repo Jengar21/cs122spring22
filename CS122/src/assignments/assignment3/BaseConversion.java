b179a303d29e328bc3ff5d6c9161c2215f2b1762
Jennifer Garcia <arisjennifer21@gmail.com>
2022-04-11 23:08:46 -0400

    assignment 4

---
+++ CS122/src/assignments/assignment3/BaseConversion.java
@@ -0,0 +1,46 @@
+package assignments.assignment3;
+
+import java.util.Scanner;
+
+public class BaseConversion {
+
+	public static void main (String[] args)
+	{
+		int base10Num;
+		int base;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Base Conversion Program");
+		System.out.println("Enter an integer: ");
+		base10Num = scan.nextInt();
+		System.out.println("Enter the base: ");
+		base = scan.nextInt();
+
+
+		// Call convert and print the answer
+		System.out.println(convert(base10Num, base));
+		scan.close();
+	}
+
+	// -------------------------------------------------
+	// Converts a base 10 number to another base.
+	// -------------------------------------------------
+	public static String convert (int num, int b)
+	{
+		int quotient; // the quotient when num is divided by base b
+		int remainder; // the remainder when num is divided by base b
+		quotient = num / b;
+		remainder = num % b;
+		if ( quotient == 0 ) //fill in base case
+		{
+			return ("" + remainder );
+		}
+		else
+		{
+			// Recursive step: the number is the base b representation of
+			// the quotient concatenated with the remainder
+			return ( convert(quotient, b) + remainder );
+		}
+	}
+}
