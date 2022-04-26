5b7bc2303bb6bbc7ac595c56a479dfdb7bdc4814
Jennifer Garcia <arisjennifer21@gmail.com>
2022-04-26 18:18:00 -0400

    Update Queue.java

--- CS122/src/classSamples/collections/Queue.java
+++ CS122/src/classSamples/collections/Queue.java
@@ -1,5 +1,42 @@
 package classSamples.collections;
 
-public class Queue {
+import java.util.LinkedList;
 
+
+public class Queue {
+	
+	public LinkedList<String> theQueue;
+	
+	public Queue() {
+		this.theQueue = new LinkedList<String>();
+	}
+
+	//---------------------------------------------
+	//Puts item on end of queue.
+	//---------------------------------------------
+	public void enqueue(String s) 
+	{
+		theQueue.addLast(s);
+	}
+	//---------------------------------------------
+	//Removes and returns object from front of queue.
+	//---------------------------------------------
+	public Object dequeue(){
+		return theQueue.removeFirst();
+	}
+	//---------------------------------------------
+	//Returns true if queue is empty.
+	//---------------------------------------------
+	public boolean isEmpty() {
+		if(theQueue.size()==0) {
+			return true;
+		}
+		return false;
+	}
+	//---------------------------------------------
+	//Returns the number of elements in the queue.
+	//---------------------------------------------
+	public String toString() {
+		return theQueue.toString();
+	}
 }
